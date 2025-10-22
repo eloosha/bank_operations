a618698e48185231ff2b0183ba19985fce51f88e
Илья Токмянин <itokm@inbox.ru>
2025-10-22 13:44:18 +0200

    lost files

---
+++ src/masks.py
@@ -0,0 +1,31 @@
+from typing import Union
+
+
+def get_mask_card_number(card_number: Union[int, str]) -> str:
+    """
+    Принимает номер карты, возвращает замаскированный номер
+    """
+    card = str(card_number)
+    if len(card) < 16:
+        return "Неверный номер"
+    start_of_number = card[:6]
+    end_of_number = card[-4:]
+    middle_of_number = "*" * (len(card) - 10)
+    masked = start_of_number + middle_of_number + end_of_number
+    parts = []
+    for i in range(0, len(masked), 4):
+        parts.append(masked[i:i + 4])
+    return " ".join(parts)
+
+
+def get_mask_account(account_number: Union[int, str]) -> str:
+    """
+    Принимает номер счета и возвращает замаскированный номер
+    """
+    number = str(account_number)
+    if len(number) < 20:
+        return "Неверный номер"
+    end_of_number = number[-4:]
+    rest_of_number = "**"
+    masked = rest_of_number + end_of_number
+    return masked
