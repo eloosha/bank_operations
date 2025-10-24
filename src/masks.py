9f346a6eece4c86f022f6945649fc604d04853ab
Илья Токмянин <itokm@inbox.ru>
2025-10-24 22:26:29 +0200

    подкорректировал masks.py

--- src/masks.py
+++ src/masks.py
@@ -4,14 +4,14 @@ def get_mask_card_number(card_number: str | int) -> str:
     """
     card = str(card_number)
     if len(card) < 16:
-        return "Неверный номер"
+        return "Неверные данные"
     start_of_number = card[:6]
     end_of_number = card[-4:]
     middle_of_number = "*" * (len(card) - 10)
     masked = start_of_number + middle_of_number + end_of_number
     parts = []
     for i in range(0, len(masked), 4):
-        parts.append(masked[i:i + 4])
+        parts.append(masked[i : i + 4])
     return " ".join(parts)
 
 
@@ -21,7 +21,7 @@ def get_mask_account(account_number: str | int) -> str:
     """
     number = str(account_number)
     if len(number) < 20:
-        return "Неверный номер"
+        return "Неверные данные"
     end_of_number = number[-4:]
     rest_of_number = "**"
     masked = rest_of_number + end_of_number
