f8de33cb2807b19f3ec3176882e1d0edfb78fb61
Илья Токмянин <itokm@inbox.ru>
2025-10-24 22:26:59 +0200

    откорректировал widget.py

--- src/widget.py
+++ src/widget.py
@@ -1,36 +1,25 @@
+from src.masks import get_mask_card_number, get_mask_account
+
+
 def mask_account_card(card_info: str) -> str:
     """
-    Принимаем тип и номер карты или счета, возвращаем замаскированный номер
+    Принимает строку с номером карты или счета,
+    возвращает замаскированный номер с использованием функций из masks.py.
     """
-    string_number = ''
-    string_name = ''
+    string_number = ""
+    string_name = ""
+
     for i in card_info:
-        if i.isnumeric():
+        if i.isdigit():
             string_number += i
         else:
             string_name += i
 
     if len(string_number) == 16:
-        start_of_number = string_number[:6]
-        end_of_number = string_number[-4:]
-        middle_of_number = "*" * (len(string_number) - 10)
-        masked = start_of_number + middle_of_number + end_of_number
-        parts = []
-        for i in range(0, len(masked), 4):
-            parts.append(masked[i:i + 4])
-        parts_combined = " ".join(parts)
-        return string_name + parts_combined
+        masked = get_mask_card_number(string_number)
     elif len(string_number) == 20:
-        end_of_number = string_number[-4:]
-        rest_of_number = "**"
-        masked = rest_of_number + end_of_number
-        return string_name + masked
+        masked = get_mask_account(string_number)
     else:
-        return 'Неверный номер'
+        return "Неверные данные"
+    return string_name.strip() + " " + masked
 
-
-def get_date(date: str) -> str:
-    """
-    Принимаем дату в определенном формате и преобразуем её в нужный для нас формат
-    """
-    return date[8:10] + '.' + date[5:7] + '.' + date[0:4]
