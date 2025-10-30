9ef231bd718d30428de0d4257d9690a6e1b69d15
Илья Токмянин <itokm@inbox.ru>
2025-10-31 00:18:52 +0200

    fixed functions

--- src/processing.py
+++ src/processing.py
@@ -1,6 +1,6 @@
 def filter_by_state(my_list: list[dict], state: str = "EXECUTED") -> list[dict]:
     """
-    Принимает список словарей, возвращает только те, у которых есть значение EXECUTED
+    Принимаем список словарей, возвращает только те, у которых есть значение EXECUTED
     """
     filtered_list = []
     for item in my_list:
@@ -11,7 +11,7 @@ def filter_by_state(my_list: list[dict], state: str = "EXECUTED") -> list[dict]:
 
 def sort_by_date(my_list: list[dict], ascending: bool = True) -> list[dict]:
     """
-    Принимает список словарей и сортирует их по дате
+    Принимаем список словарей и сортирует их по дате
     """
     sorted_list = sorted(my_list, key=lambda item: item["date"], reverse=ascending)
     return sorted_list
