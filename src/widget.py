225b67fc59521702bc147c682547418f65499957
Ilya Tokmyanin <itokm@inbox.ru>
2026-02-03 17:26:54 +0200

    подкорректировал функцию

--- src/widget.py
+++ src/widget.py
@@ -15,10 +15,13 @@ def mask_account_card(card_info: str) -> str:
         else:
             string_name += i
 
-    if len(string_number) == 16:
+    if len(string_number) >= 16:
         masked = get_mask_card_number(string_number)
-    elif len(string_number) == 20:
+
+    elif 'Счет' in string_name:
         masked = get_mask_account(string_number)
+    # elif len(string_number) == 20:
+    #     masked = get_mask_account(string_number)
     else:
         return "Неверные данные"
     return string_name.strip() + " " + masked
